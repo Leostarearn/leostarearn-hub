47e697e57b62193ba5515cf5c985b0e080260f46
Leostarearn <149958056+Leostarearn@users.noreply.github.com>
2025-06-26 03:51:37 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,10 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "_includes",
       output: "_site"
-    }
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
 };
-
